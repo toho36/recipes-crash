e0128fde22c589a5eabf815d2a26376be14e90c4
toho36 <toho0036@gmail.com>
2023-05-30 11:56:15 +0200

    adding grid

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import styled from 'styled-components';
 
 function Menu() {
   const [list, setList] = useState([]);
@@ -10,29 +11,49 @@ function Menu() {
     const check = localStorage.getItem('recipes');
     if (check) {
       setList(JSON.parse(check));
+      console.log(check);
     } else {
       const api = await fetch(
         `https://api.spoonacular.com/recipes/random?apiKey=16c7d208496b43848357dc23931ea10c&number=9`
       );
       const data = await api.json();
-      // console.log(data);
       setList(data.recipes);
       localStorage.setItem('recipes', JSON.stringify(data.recipes));
     }
   };
 
   return (
-    <div>
+    <Grid>
       {list.map((recipe) => {
         return (
-          <div>
-            <p>{recipe.title}</p>
+          <Card key={recipe.id}>
+            <h4>{recipe.title}</h4>
             <img src={recipe.image} alt={recipe.title} />
-          </div>
+            {/* <p>{recipe.tags}</p> */}
+          </Card>
         );
       })}
-    </div>
+    </Grid>
   );
 }
 
 export default Menu;
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(20rem, 1fr));
+  grid-gap: 3rem;
+`;
+const Card = styled.div`
+  img {
+    width: 100%;
+    border-radius: 2rem;
+  }
+  p {
+    text-decoration: none;
+  }
+  h4 {
+    text-align: center;
+    padding: 1rem;
+  }
+`;
