2f68728b4dcd08929a90719c113d5af5d7c4faa1
toho36 <toho0036@gmail.com>
2023-05-30 10:00:10 +0200

    fetching data

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,6 +1,17 @@
-import React from 'react';
+import { useEffect } from 'react';
 
 function Menu() {
+  useEffect(() => {
+    getRecipes();
+  }, []);
+  const getRecipes = async () => {
+    const api = await fetch(
+      `https://api.spoonacular.com/recipes/random?apiKey=16c7d208496b43848357dc23931ea10c&number=9`
+    );
+    const data = await api.json();
+    console.log(data);
+  };
+
   return <div>Menu</div>;
 }
 
